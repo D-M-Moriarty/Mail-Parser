93ab23f6f6f70580ed46b92a4e8cc98627597ff5
darrenmoriarty <dmamprop@gmail.com>
2019-03-02 19:57:22 +0000

    TESTING without feign and ribbon

--- src/main/java/com/darren/mail/parser/pbmapi/PbmApi.java
+++ src/main/java/com/darren/mail/parser/pbmapi/PbmApi.java
@@ -3,7 +3,6 @@ package com.darren.mail.parser.pbmapi;
 import com.darren.mail.parser.entity.CustomerDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,7 +11,7 @@ import java.util.List;
 @Service
 public class PbmApi {
 
-    @Autowired
+    //    @Autowired
     private PbmApiService pbmApiService;
 
     private static final Logger logger = LoggerFactory.getLogger(PbmApi.class);
--- src/main/java/com/darren/mail/parser/pbmapi/PbmApiService.java
+++ src/main/java/com/darren/mail/parser/pbmapi/PbmApiService.java
@@ -1,16 +1,15 @@
 package com.darren.mail.parser.pbmapi;
 
 import com.darren.mail.parser.entity.CustomerDetails;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name="pbm-api-service")
-@RibbonClient(name="pbm-api-service")
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
+//import org.springframework.cloud.openfeign.FeignClient;
+
 @Service
 public interface PbmApiService {
 
