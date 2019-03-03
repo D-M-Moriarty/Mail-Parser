8f2f2f5cb34297b101fa3b720d9099983144aad2
darrenmoriarty <dmamprop@gmail.com>
2019-03-03 23:39:05 +0000

    Revert "added artifactory urls to POM"
    This reverts commit 33c62fdc

--- DockerScript.sh
+++ DockerScript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mvn clean install -Dmaven.test.skip=true
 docker build -f Dockerfile -t pbm .
-docker tag pbm dmoriarty/pbm
-docker push dmoriarty/pbm
+docker tag pbm ec2-13-58-154-84.us-east-2.compute.amazonaws.com:5000/pbm
+docker push ec2-13-58-154-84.us-east-2.compute.amazonaws.com:5000/pbm
 
