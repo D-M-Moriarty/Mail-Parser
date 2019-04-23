05291a725a3ea2360f54ab2d28c5fbeee74b5b1d
darrenmoriarty <dmamprop@gmail.com>
2019-04-23 19:37:42 +0100

    break integration tests

--- src/integration-test/java/com/darren/mail/parser/mail/PbmApiIT.java
+++ src/integration-test/java/com/darren/mail/parser/mail/PbmApiIT.java
@@ -3,10 +3,7 @@ package com.darren.mail.parser.mail;
 import com.darren.mail.parser.entity.CustomerDetails;
 import com.darren.mail.parser.pbmapi.PbmApi;
 import com.darren.mail.parser.pbmapi.PbmApiService;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -16,6 +13,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -57,49 +57,50 @@ public class PbmApiIT {
     public void tearDown() {
     }
 
-//    @Test
-//    public void checkGetCustomerDetailsByEmail_valid() {
-//        when(pbmApiService.retrieveCustomerDetailsByEmail(anyString())).thenReturn(customerDetails);
-//        List<CustomerDetails> customerDetails2 = pbmApi.getCustomerDetailsByEmail(customerDetails.get(0).getEmail());
-//        Assert.assertEquals(customerDetails.get(0).getCustNo(), customerDetails2.get(0).getCustNo());
-//    }
-//
-//    @Test
-//    public void checkIsValidCustomerNumbers_valid() {
-//        when(pbmApiService.retrieveCustomerDetailsByCustomerNumber(anyString())).thenReturn(customerDetails);
-//        boolean isValid = pbmApi.isValidCustomerNumber(customerNumbers);
-//        Assert.assertTrue(isValid);
-//    }
-//
-//    @Test
-//    public void checkIsValidCustomerNumber_invalid() {
-//        List<String> customerNumbers2 = new ArrayList<>();
-//        customerNumbers2.add("09876543");
-//        when(pbmApiService.retrieveCustomerDetailsByCustomerNumber(anyString())).thenReturn(new ArrayList<>());
-//        boolean isValid = pbmApi.isValidCustomerNumber(customerNumbers2);
-//        Assert.assertFalse(isValid);
-//    }
-//
-//    @Test
-//    public void checkIsValidCustomerEmail_valid() {
-//        when(pbmApiService.retrieveCustomerDetailsByEmail(anyString())).thenReturn(customerDetails);
-//        List<CustomerDetails> customerDetails2 = pbmApi.getCustomerDetailsByEmail(customerDetails.get(0).getEmail());
-//        Assert.assertEquals(customerDetails.get(0).getCustNo(), customerDetails2.get(0).getCustNo());
-//    }
-//
-//    @Test
-//    public void checkIsValidCustomerEmail_invalid() {
-//        String email = "tim@email.com";
-//        when(pbmApiService.retrieveCustomerDetailsByEmail(anyString())).thenReturn(customerDetails);
-//        List<CustomerDetails> customerDetails2 = pbmApi.getCustomerDetailsByEmail(email);
-//        Assert.assertEquals(customerDetails.get(0), customerDetails2.get(0));
-//    }
-//
-//    @Test
-//    public void checkGetCustomerNumber() {
-//        List<String> customerNumbers2 = pbmApi.getCustomerNumbers(customerDetails);
-//        Assert.assertEquals(customerDetails.get(0).getCustNo(), customerNumbers2.get(0));
-//    }
+    @Test
+    public void checkGetCustomerDetailsByEmail_valid() {
+        when(pbmApiService.retrieveCustomerDetailsByEmail(anyString())).thenReturn(customerDetails);
+        List<CustomerDetails> customerDetails2 = pbmApi.getCustomerDetailsByEmail(customerDetails.get(0).getEmail());
+        Assert.assertEquals(customerDetails.get(0).getCustNo(), customerDetails2.get(0).getCustNo());
+        Assert.assertEquals(customerDetails.get(0).getCustNo(), "jim");
+    }
+
+    @Test
+    public void checkIsValidCustomerNumbers_valid() {
+        when(pbmApiService.retrieveCustomerDetailsByCustomerNumber(anyString())).thenReturn(customerDetails);
+        boolean isValid = pbmApi.isValidCustomerNumber(customerNumbers);
+        Assert.assertTrue(isValid);
+    }
+
+    @Test
+    public void checkIsValidCustomerNumber_invalid() {
+        List<String> customerNumbers2 = new ArrayList<>();
+        customerNumbers2.add("09876543");
+        when(pbmApiService.retrieveCustomerDetailsByCustomerNumber(anyString())).thenReturn(new ArrayList<>());
+        boolean isValid = pbmApi.isValidCustomerNumber(customerNumbers2);
+        Assert.assertFalse(isValid);
+    }
+
+    @Test
+    public void checkIsValidCustomerEmail_valid() {
+        when(pbmApiService.retrieveCustomerDetailsByEmail(anyString())).thenReturn(customerDetails);
+        List<CustomerDetails> customerDetails2 = pbmApi.getCustomerDetailsByEmail(customerDetails.get(0).getEmail());
+        Assert.assertEquals(customerDetails.get(0).getCustNo(), customerDetails2.get(0).getCustNo());
+    }
+
+    @Test
+    public void checkIsValidCustomerEmail_invalid() {
+        String email = "tim@email.com";
+        when(pbmApiService.retrieveCustomerDetailsByEmail(anyString())).thenReturn(customerDetails);
+        List<CustomerDetails> customerDetails2 = pbmApi.getCustomerDetailsByEmail(email);
+        Assert.assertEquals(customerDetails.get(0), customerDetails2.get(0));
+    }
+
+    @Test
+    public void checkGetCustomerNumber() {
+        List<String> customerNumbers2 = pbmApi.getCustomerNumbers(customerDetails);
+        Assert.assertEquals(customerDetails.get(0).getCustNo(), customerNumbers2.get(0));
+    }
 }
 
 
